01c9bca000043ddd43be67d6875bc51cd0d83e0f
Bacon41 <terrencepenner@gmail.com>
2014-03-12 10:22:25 -0600

    Added basic email sending functionality.
    This is just a template for sending the email response.

--- www/js/sendMail.js
+++ www/js/sendMail.js
@@ -1,7 +1,12 @@
-function check() {
+function sendResponse() {
 	window.plugin.email.isServiceAvailable(
 		function (isAvailable) {
-			alert(isAvailable ? 'Service is available' : 'Service NOT available');
+			window.plugin.email.open({
+				to:          ['terrencepenner@gmail.com'],
+				subject:     'H2BAC Response',
+				body:        '<h3>TEST</h3><h2>TEST</h2><h1>TEST</h1>',
+				isHtml:      true
+			});
 		}
 	);
 }
