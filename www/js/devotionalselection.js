ecd776bc212e8084e7bf9aa47e892ce0aad4a7d5
Mark Smullen <mark@techfocus.net>
2014-03-14 10:16:23 -0600

    Add layout retrieval function, stub layout function

--- www/js/devotionalselection.js
+++ www/js/devotionalselection.js
@@ -9,9 +9,36 @@ var permenantStorage = window.localStorage;
 // Support functions
 // ****************************************************************************
 
+window.onload = function() {
+	getLayout();
+};
+
 // This function takes a url and changes the current page to be it
 function gotoDevotional(page) {
 	permenantStorage.setItem("page", page);
 	
     window.location = "devotionalpages.html";
+}
+
+// This function determines the current aspect ratio of the device and
+// returns 0 if portrait, 1 if landscape
+function getLayout() {
+	var width = $(window).width();
+	var height = $(window).height();
+	var ratio = width / height;
+	var layout = (ratio < 1) ? 0 : 1;
+	alert("size: (" + width + ", " + height + ")\nratio: " + ratio + "\nlayout: " + layout);
+	
+	return layout;
+}
+
+// Set the button layout of the page
+function layoutButtons(layout) {
+	if (layout == 0) {
+		// Portrait
+		
+	} else {
+		// Landscape
+		
+	}
 }
